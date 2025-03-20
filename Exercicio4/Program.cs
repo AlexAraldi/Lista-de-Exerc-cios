50c8acec3e01de0e687793334eb0d2d840a45f4d
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-20 18:58:39 -0300

    Implementado Exercicio 4

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Exercicio4
+{
+    internal class Program
+    {
+
+        //4. Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+        // °F = °C × 1,8 + 32
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(" >>>>> Conversor de Escalas T° <<<<< ");
+            Console.WriteLine("Digite 1 para graus Celsius ");
+            Console.WriteLine("Digite 2 para graus Fahrenheit ");
+
+            int valorMenu = Convert.ToInt32(Console.ReadLine());
+
+            if (valorMenu == 1)
+            {
+                Console.Write("Escreva o valor de graus Celsius: ");
+                double celsius = Convert.ToDouble(Console.ReadLine());
+
+                double resultado = (celsius * 1.2) + 32;
+
+                Console.Write($"O valor em °F é: {resultado} F°.");
+            }
+            else 
+            {
+
+                Console.Write("Escreva o valor de Fahrenheit: ");
+                double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+                double resulto2 = (5/9) * (fahrenheit - 32);
+
+                Console.WriteLine($"O valor da T° em Celsius é: {resulto2} C°");
+
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
