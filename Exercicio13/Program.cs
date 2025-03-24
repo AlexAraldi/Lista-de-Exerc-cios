68a8f0a674433510c1c6a85f90148c9ceb232cb2
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-24 12:00:42 -0300

    Implementado Exercicio14

--- Exercicio13/Program.cs
+++ Exercicio13/Program.cs
@@ -5,7 +5,24 @@
         //Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Calculadora");
+
+            Console.WriteLine("Escreva o número A: ");
+            int nunA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escreva o número B: ");
+            int nunB = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escreva o numero C: ");
+            int nunC = Convert.ToInt32(Console.ReadLine());
+
+            if (nunA + nunB < nunC) 
+            {
+                Console.WriteLine(" Sim, a soma de A + B é menor que C.");
+            }
+            else Console.WriteLine("Não, a soma é maior que C");
+
+                Console.ReadLine();
         }
     }
 }
---
+++ Exercicio14/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Exercicio14
+{
+    internal class Program
+    {
+        //14. Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Amostra de números"); 
+
+            Console.WriteLine("Escreva o primeiro número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escreva o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escreva o terceiro número: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (num1 > num2 && num1 > num3 && num3 > num2)
+            {
+                Console.WriteLine($" A sequencia correta é: { num1 } >{ num2 } , { num3 }.");
+            }
+            else if (num2 > num3 && num2 > num1 && num2)
+            {
+                Console.WriteLine(num2);
+            }
+            else if (num3 > num2 && num3 > num1)
+            {
+                Console.WriteLine(num3);
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
