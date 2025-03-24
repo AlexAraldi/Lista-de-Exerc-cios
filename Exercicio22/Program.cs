677cdc7b20870e2312dc73eec400a22f67e4df25
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-24 19:33:34 -0300

    Implementado Exercicio23

---
+++ Exercicio22/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Exercicio22
+{
+    internal class Program
+    {
+        // 22. Escreva um programa que leia um número e imprima a sequência de Fibonacci até esse número.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Exercicio23/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercicio23
+{
+    internal class Program
+    {
+        //23. Escreva um programa que imprima os números de 1 a 100 em ordem crescente, substituindo os números
+        //    múltiplos de 3 pela palavra &quot;Fizz&quot; e os múltiplos de 5 pela palavra &quot;Buzz&quot;. Para números que são múltiplos de
+        //    ambos, use &quot;FizzBuzz&quot;.
+        static void Main(string[] args)
+        {
+            string numeros = "";
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    numeros = "FizzBuzz";
+                    Console.WriteLine(numeros);
+                }
+                else if (i % 3 == 0)
+                {
+                    numeros = "Fizz";
+                    Console.WriteLine(numeros);
+                }
+                else if (i % 5 == 0)
+                {
+                    numeros = "Buzz";
+                    Console.WriteLine(numeros);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
