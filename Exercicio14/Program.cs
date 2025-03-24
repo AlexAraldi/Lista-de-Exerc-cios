46cba2a7d023a4cd093e8574e6a2a17b1e9a9605
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-24 12:04:21 -0300

    Retificado String Resultado

--- Exercicio14/Program.cs
+++ Exercicio14/Program.cs
@@ -7,27 +7,29 @@
         {
             Console.WriteLine("Amostra de números"); 
 
-            Console.WriteLine("Escreva o primeiro número: ");
+            Console.Write("Escreva o primeiro número: ");
             int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Escreva o segundo número: ");
+            Console.Write("Escreva o segundo número: ");
             int num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Escreva o terceiro número: ");
+            Console.Write("Escreva o terceiro número: ");
             int num3 = Convert.ToInt32(Console.ReadLine());
 
 
-            if (num1 > num2 && num1 > num3 && num3 > num2)
+            if (num1 > num2 && num2 > num3 )
             {
-                Console.WriteLine($" A sequencia correta é: { num1 } >{ num2 } , { num3 }.");
+                Console.WriteLine($" A sequencia correta é: { num1 } > { num2 } > { num3 }.");
             }
-            else if (num2 > num3 && num2 > num1 && num2)
+            else if (num2 > num1 && num2 > num3)
             {
-                Console.WriteLine(num2);
+                Console.WriteLine($" A sequencia correta é: {num2} > {num3} > {num1}.");
+
             }
-            else if (num3 > num2 && num3 > num1)
+            else if (num3 > num1 && num3 > num2)
             {
-                Console.WriteLine(num3);
+                Console.WriteLine($" A sequencia correta é: {num3} > {num2} > {num1}.");
+
             }
 
 
