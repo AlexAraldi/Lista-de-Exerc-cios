e140c22aa08aca5416515e369cda626d518df0f4
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-24 19:13:43 -0300

    Implementado Exercicio13

--- Exercicio13/Program.cs
+++ Exercicio13/Program.cs
@@ -3,6 +3,7 @@
     internal class Program
     {
         // 13 - Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+
         static void Main(string[] args)
         {
             Console.WriteLine("Calculadora");
---
+++ Exercicio21/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio21
+{
+    internal class Program
+    {
+        // 21. Escreva um algoritmo que leia um valor inicial A e imprima a sequência de valores do cálculo de A! e o seu
+        // resultado.
+        // a.Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120
+        // b.Pesquise sobre “fatorial”
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
