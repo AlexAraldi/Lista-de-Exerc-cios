59e4bb8695e82ae42aebc8f791a41d9764a64422
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-24 10:58:31 -0300

    Implementado Exercicio13

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicio13
+{
+    internal class Program
+    {
+        //Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
